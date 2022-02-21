ed815f811f89173e92cafb91003c31a191c1c348
LingruiZhang <jerryzhang94@outlook.com>
2022-02-21 02:36:08 +0000

    Initial The Project - Step 3

--- finder/urls.py
+++ finder/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from finder import views
 app_name = 'finder'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name='index'),
+    path("about/", views.about, name='about'),
 ]
